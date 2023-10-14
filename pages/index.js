2f4114dde1bb907dd68e410f1b7b8f972c4e0419
Rishav <51906186+rishavmahapatra@users.noreply.github.com>
2023-10-14 23:45:51 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -109,7 +109,7 @@ export default function Home() {
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-slate-600 flex-1">
               <Image src={code} width={100} height={100} />
               <h3 className="text-lg font-medium pt-8 pb-2 dark:text-white">
-                BackEnd Development
+                Back-End Development
                </h3>
               <p className="py-2 dark:text-white">
                 Do you have an idea for your next great website? Lets make it a
