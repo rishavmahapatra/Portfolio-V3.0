c1140a19a26cae113c9ff8da4205e57536f29b89
rishavmahapatra <rishavmahapatra24@gmail.com>
2024-08-31 00:37:03 +0530

    responsive design

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,15 @@ import {
   AiFillLinkedin,
   AiFillGithub,
 } from "react-icons/ai";
+
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { useState } from "react";
-import dev from "../public/dev-ed-wave.jpeg";
+import dev from "../public/rishav.png";
 import code from "../public/code.png";
-import pro from "../public/1.png";
-
 import design from "../public/design.png";
 import consulting from "../public/consulting.png";
 import Image from "next/image";
-import { motion, useScroll } from "framer-motion";
+import { motion } from "framer-motion";
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(true);
@@ -25,44 +24,42 @@ export default function Home() {
         <meta name="description" content="Front-End Developer Portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="overflow-hidden bg-neutral-100 px-5 dark:bg-neutral-900 md:px-10 lg:px-20">
-        {/* min-h-full sm: */}
-        <div className="min-h-screen">
-          <nav className="sticky top-0  py-2 mt-7 flex justify-between dark:text-white">
-            <h1 className="font-sans text-xl"></h1>
-            <motion.ul
-              className="flex items-center"
-              // initial={{ y:-100,opacity: 0 }}
-              // animate={{ y:1, opacity: 1 }}
-              // transition={{duration:0.5}}
 
-              initial={{ y: 100 }}
-              animate={{ y: 0 }}
-              transition={{ type: "spring", stiffness: 110 }}
-            >
+      <main className="overflow-hidden bg-stone-100 px-5 dark:bg-stone-900 md:px-10 lg:px-20">
+        <div className="min-h-screen">
+          <nav className="fixed top-0 left-0 right-0 z-10  backdrop-blur-md shadow-indigo-200 shadow-sm p-3 flex justify-end lg:justify-between font-mono text-teal-900  dark:text-teal-200">
+            <ul className="hidden lg:flex">
               <li>
                 <a
-                  className="bg-gradient-to-b from-indigo-500  to-indigo-700 font-mono text-teal-200   px-4 py-2 border-none rounded-md ml-8"
-                  // href="#skills"
+                  className="cursor-pointer px-4 py-2 border-none rounded-md ml-8"
                   onClick={() => {
-                    const anchor = document.querySelector('#skills')
-                    anchor.scrollIntoView({ behavior: 'spin', block: 'start' })
-                   }}
+                    const anchor = document.querySelector("#home");
+                    anchor.scrollIntoView({
+                      behavior: "smooth",
+                      block: "start",
+                    });
+                  }}
                 >
-                  Skills
+                  Home
                 </a>
+              </li>
+              <li>
                 <a
-                  className=" font-mono text-teal-200   px-4 py-2 border-none rounded-md ml-8"
-                  href="#skills"
-                  // onClick={() => {
-                  //   const anchor = document.querySelector('#skills')
-                  //   anchor.scrollIntoView({ behavior: 'smooth', block: 'center' })
-                  //  }}
+                  className="cursor-pointer px-4 py-2 border-none rounded-md ml-8"
+                  onClick={() => {
+                    const anchor = document.querySelector("#skills");
+                    anchor.scrollIntoView({
+                      behavior: "smooth",
+                      block: "start",
+                    });
+                  }}
                 >
                   Skills
                 </a>
               </li>
+            </ul>
 
+            <ul className="flex items-center gap-4">
               <li>
                 <BsFillMoonStarsFill
                   onClick={() => setDarkMode(!darkMode)}
@@ -71,69 +68,59 @@ export default function Home() {
               </li>
               <li>
                 <a
-                  className="bg-gradient-to-b from-indigo-500  to-indigo-700 font-mono text-teal-200   px-4 py-2 border-none rounded-md ml-8"
+                  className="bg-gradient-to-b from-indigo-500  to-indigo-800 font-mono text-teal-200  px-3 py-1 border-none rounded-md mr-6"
                   href="https://drive.google.com/file/d/1FVSz3BrtfaAqCgS_7-yT-ed62HEjWO-l/view?usp=sharing"
                 >
                   Resume
                 </a>
               </li>
-            </motion.ul>
+            </ul>
           </nav>
-          <section className="mt-32 sm:mt-14 lg:mt-28 justify-center">
+          {/* mt-48 sm:mt-28 lg:mt-36 */}
+          <section id="home" className="absolute top-0 left-0 right-0 ">
             <motion.div
-              className="text-center"
+              className="text-center mt-40"
               initial={{ x: -100, opacity: 0 }}
               whileInView={{ x: 0, opacity: 1 }}
-              viewport={{once:true}}
-              // transition={{ duration: 0.4 }}
+              viewport={{ once: false }}
             >
-              <h2 className=" text-2xl md:text-5xl mx-auto max-w-2xl antialiased py-2 text-teal-700 font-semibold dark:text-indigo-400 ">
+              <h2 className=" text-2xl md:text-5xl mx-auto max-w-2xl antialiased py-2 text-teal-800 font-semibold dark:text-indigo-400 ">
                 Hi👋, I am Rishav Mahapatra
               </h2>
-              <h3 className=" text-lg md:text-3xl max-w-xl mx-auto antialiased  py-2 font-mono font-medium text-cyan-700 dark:text-teal-200  hover:scale-110">
+              <h3 className=" text-lg md:text-3xl max-w-xl mx-auto antialiased  py-2 font-mono font-medium text-cyan-900 dark:text-teal-200 ">
                 I am a Front-End Developer
               </h3>
-              {/* <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
-              Developer providing services for programming and design content
-              needs. Join me down below!
-            </p> */}
-              {/* bg-gradient-to-b from-indigo-400 */}
               <motion.div
-                className=" m-4 mx-auto bg-transparent rounded-full w-52 h-52 md:w-64 md:h-64 relative overflow-hidden"
-                initial={{ y: -500, opacity: 0 }}
+                initial={{ y: 200, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
-                transition={{ delay: 0.6, type: "spring", stiffness: 190 }}
+                transition={{ delay: 0.2 }}
               >
-                <Image src={dev} layout="fill" objectFit="cover" />
-              </motion.div>
-              <motion.div
-                className="cursor-pointer my-6 py-2 max-w-xs mx-auto text-4xl md:text-6xl flex justify-center gap-8 sm:gap-14 text-gray-700 dark:text-gray-300"
-                initial={{ y: 100, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                transition={{ delay: 0.4, duration: 0.4 }}
-              >
-                <AiFillTwitterCircle className="hover:scale-110 hover:text-indigo-400" />
-                <AiFillLinkedin className="hover:scale-110 hover:text-indigo-400" />
-                <AiFillGithub className="hover:scale-110 hover:text-indigo-400" />
+                <div className=" m-4 mx-auto bg-transparent rounded-full w-52 h-52 md:w-64 md:h-64 relative overflow-hidden">
+                  <Image src={dev} className="object-cover w-full h-auto" />
+                </div>
+                <div className="cursor-pointer my-6 py-2 max-w-xs mx-auto text-4xl md:text-5xl flex justify-center gap-8 sm:gap-10 text-gray-700 dark:text-gray-300">
+                  <AiFillLinkedin className="hover:scale-110 hover:text-indigo-500" />
+                  <AiFillGithub className="hover:scale-110 hover:text-indigo-500" />
+                </div>
               </motion.div>
             </motion.div>
           </section>
         </div>
 
-        <section id="skills" className="text-left">
+        <section id="skills" className=" text-left scroll-m-14">
           <div>
             <h3 className="text-4xl py-1 dark:text-white">Skills</h3>
-            <p className="font-sans text-md py-2 leading-8 text-gray-800 dark:text-gray-200 mx-auto md:text-xl">
+            {/* <p className="font-sans text-md py-2 leading-8 text-gray-800 dark:text-gray-200 mx-auto md:text-xl">
               PS- I have gained these skills after years of experience and not
               just after completing one project/tutorial.
-            </p>
+            </p> */}
           </div>
           <div className="lg:flex gap-10">
             <motion.div
               className="text-left shadow-lg p-10 rounded-xl my-10 bg-cyan-200 dark:bg-slate-600 flex-1"
               initial={{ x: 100, opacity: 0 }}
               whileInView={{ x: 0, opacity: 1 }}
-              viewport={{once:true}}
+              viewport={{ once: false }}
               // transition={{ duration: 0.4 }}
             >
               <Image src={design} width={100} height={100} />
@@ -156,7 +143,7 @@ export default function Home() {
               className=" shadow-lg p-10 rounded-xl my-10 bg-teal-200 dark:bg-slate-600 flex-1"
               initial={{ x: 100, opacity: 0 }}
               whileInView={{ x: 0, opacity: 1 }}
-              viewport={{once:true}}
+              viewport={{ once: false }}
               // transition={{ duration: 0.4 }}
             >
               <Image src={code} width={100} height={100} />
@@ -179,7 +166,7 @@ export default function Home() {
               className="shadow-lg p-10 rounded-xl my-10 bg-blue-200 dark:bg-slate-600 flex-1"
               initial={{ x: 100, opacity: 0 }}
               whileInView={{ x: 0, opacity: 1 }}
-              viewport={{once:true}}
+              viewport={{ once: false }}
               // transition={{ duration: 0.4 }}
             >
               <Image src={consulting} width={100} height={100} />
