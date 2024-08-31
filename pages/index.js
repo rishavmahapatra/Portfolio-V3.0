43b8cc8e7e1cbf2cad1ea07c35c4753125f17c66
rishavmahapatra <rishavmahapatra24@gmail.com>
2024-09-01 03:13:54 +0530

    correction, min-h-screen only above lg screen

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
       </Head>
 
       <main className="overflow-hidden bg-neutral-100 px-5 dark:bg-neutral-900 md:px-10 lg:px-20">
-        <div className="h-screen">
+        <div className="lg:h-screen">
           <nav className="fixed top-0 left-0 right-0 z-10 backdrop-blur-xl md:dark:shadow-stone-800 shadow-sm p-3 flex justify-end lg:justify-between font-mono text-teal-900  dark:text-teal-200">
             <ul className="hidden lg:flex">
               <li>
@@ -45,7 +45,7 @@ export default function Home() {
                     });
                   }}
                 >
-                  🔴Home
+                  Home
                 </a>
               </li>
               <li>
@@ -59,7 +59,7 @@ export default function Home() {
                     });
                   }}
                 >
-                  🟡Skills
+                  Skills
                 </a>
               </li>
               <li>
@@ -73,7 +73,7 @@ export default function Home() {
                     });
                   }}
                 >
-                 🟢Projects
+                 Projects
                 </a>
               </li>
             </ul>
