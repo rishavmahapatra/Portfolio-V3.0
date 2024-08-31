7bd1722b5b43a33d72320a51e87693cc709adf70
rishavmahapatra <rishavmahapatra24@gmail.com>
2024-09-01 03:09:53 +0530

    added skills

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import Head from "next/head";
 import {
-  AiFillTwitterCircle,
   AiFillLinkedin,
   AiFillGithub,
 } from "react-icons/ai";
+import { FaJava } from "react-icons/fa";
+import { IoLogoFirebase } from "react-icons/io5";
+
+import { SiNextdotjs,SiCss3 ,SiReact,SiBootstrap,SiTailwindcss,SiJavascript,SiHtml5,SiNodedotjs  } from "react-icons/si";
+import framer from "../public/framer-motion.svg"
+import shadcn from "../public/shadcn-ui.svg"
 
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { useState } from "react";
@@ -25,13 +30,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="overflow-hidden bg-stone-100 px-5 dark:bg-stone-900 md:px-10 lg:px-20">
-        <div className="min-h-screen">
-          <nav className="fixed top-0 left-0 right-0 z-10  backdrop-blur-xl md:shadow-indigo-200 shadow-sm p-3 flex justify-end lg:justify-between font-mono text-teal-900  dark:text-teal-200">
+      <main className="overflow-hidden bg-neutral-100 px-5 dark:bg-neutral-900 md:px-10 lg:px-20">
+        <div className="h-screen">
+          <nav className="fixed top-0 left-0 right-0 z-10 backdrop-blur-xl md:dark:shadow-stone-800 shadow-sm p-3 flex justify-end lg:justify-between font-mono text-teal-900  dark:text-teal-200">
             <ul className="hidden lg:flex">
               <li>
                 <a
-                  className="cursor-pointer px-4 py-2 border-none rounded-md ml-8"
+                  className="cursor-pointer p-3 border-none rounded-md"
                   onClick={() => {
                     const anchor = document.querySelector("#home");
                     anchor.scrollIntoView({
@@ -40,12 +45,12 @@ export default function Home() {
                     });
                   }}
                 >
-                  Home
+                  🔴Home
                 </a>
               </li>
               <li>
                 <a
-                  className="cursor-pointer px-4 py-2 border-none rounded-md ml-8"
+                  className="cursor-pointer p-3 border-none rounded-md "
                   onClick={() => {
                     const anchor = document.querySelector("#skills");
                     anchor.scrollIntoView({
@@ -54,7 +59,21 @@ export default function Home() {
                     });
                   }}
                 >
-                  Skills
+                  🟡Skills
+                </a>
+              </li>
+              <li>
+                <a
+                  className="cursor-pointer p-3 border-none rounded-md "
+                  onClick={() => {
+                    const anchor = document.querySelector("#skills");
+                    anchor.scrollIntoView({
+                      behavior: "smooth",
+                      block: "start",
+                    });
+                  }}
+                >
+                 🟢Projects
                 </a>
               </li>
             </ul>
@@ -68,7 +87,7 @@ export default function Home() {
               </li>
               <li>
                 <a
-                  className="bg-gradient-to-b from-indigo-500  to-indigo-800 font-mono text-teal-200  px-3 py-1 border-none rounded-md md:mr-4"
+                  className="bg-indigo-500  text-teal-200  px-3 py-1 border-none rounded-md md:mr-4"
                   href="https://drive.google.com/file/d/1FVSz3BrtfaAqCgS_7-yT-ed62HEjWO-l/view?usp=sharing"
                 >
                   Resume
@@ -84,15 +103,15 @@ export default function Home() {
               whileInView={{ x: 0, opacity: 1 }}
               viewport={{ once: false }}
             >
-              <h2 className=" text-2xl md:text-5xl mx-auto max-w-2xl antialiased py-2 text-teal-800 font-semibold dark:text-indigo-400 ">
+              <h2 className="text-lg md:text-3xl max-w-3xl mx-auto  antialiased py-2  font-semibold text-teal-800 dark:text-indigo-400 ">
                 Hi👋, I am Rishav Mahapatra
               </h2>
-              <h3 className=" text-lg md:text-3xl max-w-xl mx-auto antialiased  py-2 font-mono font-medium text-cyan-900 dark:text-teal-200 ">
-                I am a Front-End Developer
+              <h3 className="text-xl md:text-3xl mx-auto max-w-3xl antialiased  py-2 font-mono font-medium text-cyan-900 dark:text-teal-200 ">
+                I am a Front-End/UI Developer
               </h3>
               <motion.div
-                initial={{ y: 200, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
+                initial={{ x:-100, opacity: 0 }}
+                animate={{ x:0, opacity: 1 }}
                 transition={{ delay: 0.2 }}
               >
                 <div className=" m-4 mx-auto bg-transparent rounded-full w-52 h-52 md:w-64 md:h-64 relative overflow-hidden">
@@ -101,15 +120,16 @@ export default function Home() {
                 <div className="cursor-pointer my-6 py-2 max-w-xs mx-auto text-4xl md:text-5xl flex justify-center gap-8 sm:gap-10 text-gray-700 dark:text-gray-300">
                   <AiFillLinkedin className="hover:scale-110 hover:text-indigo-500" />
                   <AiFillGithub className="hover:scale-110 hover:text-indigo-500" />
+                  
                 </div>
               </motion.div>
             </motion.div>
           </section>
         </div>
 
-        <section id="skills" className=" text-left scroll-m-14">
+        <section id="skills" className=" text-center scroll-m-14">
           <div>
-            <h3 className="text-4xl py-1 dark:text-white">Skills</h3>
+            <h3 className="text-4xl text-left py-1 text-cyan-900 dark:text-teal-200">Skills</h3>
             {/* <p className="font-sans text-md py-2 leading-8 text-gray-800 dark:text-gray-200 mx-auto md:text-xl">
               PS- I have gained these skills after years of experience and not
               just after completing one project/tutorial.
@@ -117,50 +137,36 @@ export default function Home() {
           </div>
           <div className="lg:flex gap-10">
             <motion.div
-              className="text-left shadow-lg p-10 rounded-xl my-10 bg-cyan-200 dark:bg-slate-600 flex-1"
+              className=" shadow-lg p-10 rounded-xl my-10 bg-cyan-200 dark:bg-slate-600 flex-1"
               initial={{ x: 100, opacity: 0 }}
               whileInView={{ x: 0, opacity: 1 }}
               viewport={{ once: false }}
-              // transition={{ duration: 0.4 }}
             >
-              <Image src={design} width={100} height={100} />
+             {/* <Image src={code} width={100} height={100} />  */}
               <h3 className=" text-lg font-medium pt-8 pb-2 dark:text-white ">
-                Front-End Development
+                Programming Languages
               </h3>
-              <p className="py-2 dark:text-white">
-                Creating elegant designs suited for your needs following core
-                design theory.
-              </p>
-              <h4 className="py-4 text-teal-600 dark:text-teal-400">
-                Frontend Tools I Use
-              </h4>
-              <p className="text-gray-800 py-1 dark:text-white">ReactJS</p>
-              <p className="text-gray-800 py-1 dark:text-white">NextJS</p>
-              <p className="text-gray-800 py-1 dark:text-white">Tailwind</p>
-              <p className="text-gray-800 py-1 dark:text-white">Firebase</p>
+              <p className="flex  mx-auto justify-left items-center text-gray-800 py-1 dark:text-cyan-300">{<SiJavascript  className="text-yellow-400 text-3xl mr-3 " />}JAVASCRIPT </p>
+              <p className="flex  mx-auto justify-left items-center text-gray-800 py-1 dark:text-cyan-300">{<SiHtml5  className="text-orange-500 text-3xl mr-3" />} HTML </p>
+              <p className="flex  mx-auto justify-left items-center text-gray-800 py-1 dark:text-cyan-300">{<SiCss3 className="text-blue-400 text-3xl mr-3 " />}CSS </p>
+              <p className="flex  mx-auto justify-left items-center text-gray-800 py-1 dark:text-cyan-300">{<FaJava   className="text-orange-500 text-3xl mr-3 " />}JAVA </p>
             </motion.div>
             <motion.div
               className=" shadow-lg p-10 rounded-xl my-10 bg-teal-200 dark:bg-slate-600 flex-1"
               initial={{ x: 100, opacity: 0 }}
               whileInView={{ x: 0, opacity: 1 }}
               viewport={{ once: false }}
-              // transition={{ duration: 0.4 }}
             >
-              <Image src={code} width={100} height={100} />
+              {/* <Image src={design} width={100} height={100} /> */}
               <h3 className="text-lg font-medium pt-8 pb-2 dark:text-white">
-                Back-End Development
+                Front-End Frameworks
               </h3>
-              <p className="py-2 dark:text-white">
-                Do you have an idea for your next great website? Lets make it a
-                reality.
-              </p>
-              <h4 className="py-4 text-teal-600 dark:text-teal-400">
-                Backend Tools I Use
-              </h4>
-              <p className="text-gray-800 py-1 dark:text-white">Spring</p>
-              <p className="text-gray-800 py-1 dark:text-white">Spring-Boot</p>
-              <p className="text-gray-800 py-1 dark:text-white">Mysql</p>
-              <p className="text-gray-800 py-1 dark:text-white">Azure</p>
+              
+              <p className="flex  mx-auto justify-left items-center text-gray-800 py-1 dark:text-cyan-300">{<SiReact  className="text-blue-400 text-3xl mr-3 " />}REACT.JS </p>
+              <p className="flex  mx-auto justify-left items-center text-gray-800 py-1 dark:text-cyan-300">{<SiNextdotjs className="  text-zinc-900 text-3xl mr-3 " />}NEXT.JS </p>
+              <p className="flex  mx-auto justify-left items-center text-gray-800 py-1 dark:text-cyan-300">{<SiTailwindcss className="text-blue-400 text-3xl mr-3 " />}TAILWIND CSS</p>
+              <p className="flex  mx-auto justify-left items-center text-gray-800 py-1 dark:text-cyan-300">{<SiBootstrap  className="text-violet-400 text-3xl mr-3 " />}BOOTSTRAP </p>
+
             </motion.div>
             <motion.div
               className="shadow-lg p-10 rounded-xl my-10 bg-blue-200 dark:bg-slate-600 flex-1"
@@ -169,21 +175,15 @@ export default function Home() {
               viewport={{ once: false }}
               // transition={{ duration: 0.4 }}
             >
-              <Image src={consulting} width={100} height={100} />
+              {/* <Image src={consulting} width={100} height={100} /> */}
               <h3 className="text-lg font-medium pt-8 pb-2 dark:text-white">
                 Other Dev Tools
               </h3>
-              <p className="py-2 dark:text-white">
-                Are you interested in feedback for your current project? I can
-                give you tips and tricks to level it up.
-              </p>
-              <h4 className="py-4 text-teal-600 dark:text-teal-400">
-                Design Tools I Use
-              </h4>
-              <p className="text-gray-800 py-1 dark:text-white">Photoshop</p>
-              <p className="text-gray-800 py-1 dark:text-white">Illustrator</p>
-              <p className="text-gray-800 py-1 dark:text-white">Figma</p>
-              <p className="text-gray-800 py-1 dark:text-white">Indesign</p>
+              <p className="flex  mx-auto justify-left items-center text-gray-800 py-1 dark:text-cyan-300">{<Image src={framer}  className="w-6 ml-1 mr-3" />}FRAMER-MOTION </p>
+              <p className="flex  mx-auto justify-left items-center text-gray-800 py-1 dark:text-cyan-300">{<Image src={shadcn}  className="bg-white rounded-md w-6 ml-1 mr-3" />}SHADCN-UI</p>
+              <p className="flex mx-auto justify-left items-center text-gray-800 py-1 dark:text-cyan-300">{<IoLogoFirebase className="text-yellow-500 text-3xl mr-3 " />}FIREBASE </p>
+              <p className="flex mx-auto justify-left items-center text-gray-800 py-1 dark:text-cyan-300">{<SiNodedotjs className="text-green-500 text-3xl mr-3 " />}NODE.JS</p>
+
             </motion.div>
           </div>
         </section>
