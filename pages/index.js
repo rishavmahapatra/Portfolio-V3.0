0fd8346dfb9cd5ddd7fc1861598f5ec69fefcc95
rishavmahapatra <rishavmahapatra24@gmail.com>
2024-10-09 03:45:26 +0530

    heading fix

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ import shadcn from "../public/shadcn-ui.svg"
 
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { useState } from "react";
-import dev from "../public/12.png";
+import dev from "../public/4.webp";
 import code from "../public/code.png";
 import design from "../public/design.png";
 import consulting from "../public/consulting.png";
@@ -31,13 +31,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="overflow-hidden bg-neutral-100 px-5 dark:bg-neutral-900">
+      <main className="overflow-hidden bg-neutral-100 dark:bg-gradient-to-br from-gray-900 to-black px-5 ">
         <div >
           <nav className="fixed top-0 left-0 right-0 z-10 backdrop-blur-xl md:dark:shadow-stone-800 shadow-sm p-3 flex justify-end md:justify-between font-mono text-teal-900  dark:text-teal-200">
             <ul className="hidden md:flex">
               <li>
                 <a
-                  className="cursor-pointer p-3 border-none rounded-md"
+                  className="cursor-pointer text-lg p-3 border-none rounded-md"
                   onClick={() => {
                     const anchor = document.querySelector("#home");
                     anchor.scrollIntoView({
@@ -51,7 +51,7 @@ export default function Home() {
               </li>
               <li>
                 <a
-                  className="cursor-pointer p-3 border-none rounded-md "
+                  className="cursor-pointer text-lg p-3 border-none rounded-md "
                   onClick={() => {
                     const anchor = document.querySelector("#skills");
                     anchor.scrollIntoView({
@@ -93,11 +93,11 @@ export default function Home() {
               // animate = {{ y:[200,0],scale:[0,1]}} 
               transition={{duration:0.4}}
             >
-              <h3 className="text-md md:text-4xl max-w-3xl mx-auto antialiased py-2  font-semibold text-teal-800 dark:text-purple-300 ">
+              <h3 className="text-lg md:text-4xl mx-auto tracking-tight antialiased py-2  font-bold text-teal-800 dark:text-gray-100 ">
                 Hi👋, I am Rishav Mahapatra
               </h3>
-              <h2 className="text-lg md:text-5xl mx-auto max-w-3xl tracking-tight antialiased py-2 font-semibold text-cyan-900 dark:text-teal-200 ">
-                I am a Front-End/UI Developer
+              <h2 className="text-lg md:text-5xl mx-auto mb-8  antialiased py-2 font-extrabold text-cyan-900 dark:text-gray-100">
+              I am a <span className="dark:text-indigo-300">Front-End/UI Developer</span>
               </h2>
               <motion.div
                 initial={{ y:100,opacity: 0 }}
@@ -107,10 +107,10 @@ export default function Home() {
                 <div className=" m-4 mx-auto bg-gradient-to-b from-blue-400 to-purple-200 ring-2 rounded-full w-52 h-52 md:w-64 md:h-64 relative overflow-hidden">
                   <Image src={dev} alt="profile-pic" className="objext-cover" />
                 </div>
-                <div className="cursor-pointer my-6 py-2 max-w-xs mx-auto text-4xl md:text-5xl flex justify-center gap-6 text-gray-700 dark:text-gray-300">
+                <div className="cursor-pointer my-6 py-2 max-w-xs mx-auto text-4xl md:text-5xl flex justify-center gap-6 text-gray-700 dark:text-gray-100">
                 {/* hover:text-indigo-500 */}
-                  <a href="https://www.linkedin.com/in/rishav-mahapatra/" target="_blank" rel="noreferrer"><AiFillLinkedin className="hover:scale-110" /></a>
-                  <a href="https://github.com/rishavmahapatra/" target="_blank" rel="noreferrer"><AiFillGithub className="hover:scale-110" /></a>
+                  <a href="https://www.linkedin.com/in/rishav-mahapatra/" target="_blank" rel="noreferrer"><AiFillLinkedin className="hover:scale-110 hover:text-indigo-400" /></a>
+                  <a href="https://github.com/rishavmahapatra/" target="_blank" rel="noreferrer"><AiFillGithub className="hover:scale-110 hover:text-indigo-400" /></a>
                 </div>
               </motion.div>
             </motion.div>
@@ -119,17 +119,17 @@ export default function Home() {
 
         <section id="skills" className="text-left scroll-m-14">
           <div>
-            <h3 className="text-3xl text-left py-1 text-cyan-900 dark:text-teal-200">Skills</h3>
-            <p className="font-sans text-md py-2 leading-8 text-gray-800 dark:text-gray-200 mx-auto md:text-xl">
+            <h3 className="text-lg md:text-4xl text-left py-1  font-bold text-teal-800 dark:text-gray-100">Skills</h3>
+            <p className="font-sans text-md py-2 leading-7  text-gray-800 dark:text-gray-200 mx-auto md:text-xl">
               I love to try out different libraries and frameworks available in the web. I follow different Dev communities and channels to keep myself updated with latest Tech trends. These are some skill set which I most frequently use.
             </p>
           </div>
           <div className="lg:flex gap-11">
             <motion.div
-              className=" shadow-lg p-10 rounded-xl my-14 bg-blue-200 dark:bg-slate-600 flex-1"
+              className=" shadow-lg p-10 rounded-xl my-6 bg-blue-200 dark:bg-slate-600 flex-1"
               initial={{ x: 100, opacity: 0 }}
               whileInView={{ x: 0, opacity: 1 }}
-              viewport={{ once: false }}
+              viewport={{ once: true }}
               // transition={{ duration: 0.3 }}
             >
              {/* <Image src={code} width={100} height={100} />  */}
@@ -142,10 +142,10 @@ export default function Home() {
               <p className="flex  mx-auto justify-left items-center text-gray-800 py-1 dark:text-cyan-300">{<FaJava   className="bg-slate-200 rounded-md text-yellow-800 border-2 text-3xl mr-5 " />}JAVA </p>
             </motion.div>
             <motion.div
-              className=" shadow-lg p-10 rounded-xl my-14 bg-blue-200 dark:bg-slate-600 flex-1"
+              className=" shadow-lg p-10 rounded-xl my-6 bg-blue-200 dark:bg-slate-600 flex-1"
               initial={{ x: 100, opacity: 0 }}
               whileInView={{ x: 0, opacity: 1 }}
-              viewport={{ once: false }}
+              viewport={{ once: true }}
               // transition={{ duration: 0.3 }}
             >
               {/* <Image src={design} width={100} height={100} /> */}
@@ -160,10 +160,10 @@ export default function Home() {
 
             </motion.div>
             <motion.div
-              className="shadow-lg p-10 rounded-xl my-14 bg-blue-200 dark:bg-slate-600 flex-1"
+              className="shadow-lg p-10 rounded-xl my-6 bg-blue-200 dark:bg-slate-600 flex-1"
               initial={{ x: 100, opacity: 0 }}
               whileInView={{ x: 0, opacity: 1 }}
-              viewport={{ once: false }}
+              viewport={{ once: true }}
               // transition={{ duration: 0.3 }}
             >
               {/* <Image src={consulting} width={100} height={100} /> */}
